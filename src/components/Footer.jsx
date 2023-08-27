0f8ac76e5a819aa5509afb1b93c8a00be3f2a514
Student9876 <shouvik9876@gmail.com>
2023-08-28 02:08:50 +0530

    Add Carousel in HeadingDiv and Fixed Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,11 @@ function Footer() {
     return <>
         <div className="footerDiv">
             <center><h3>Connect With Us</h3><p>Social Media Profiles</p>
-
                 <table>
                     <tr>
-                        <td><a href="https://instagram.com/gdsc_iiitbhopal?igshid=MzRlODBiNWFlZA==" target="_blank" rel="noopener noreferrer"><img src={instaImg} alt="Instagram_Profile" /></a></td>
-                        <td><a href="https://www.linkedin.com/company/dsc-iiit-bhopal/" target="_blank" rel="noopener noreferrer"><img src={linkedInImg} alt="linkedIn_Profile" /></a></td>
-                        <td><a href="https://twitter.com/gdsc_iiitbhopal?t=4NmbYiJ7qfs2eG6RomRGqg&s=09" target="_blank" rel="noopener noreferrer"><img src={twitterImg} alt="Twitter_profile" /></a></td>
+                        <td><a href="https://instagram.com/gdsc_iiitbhopal?igshid=MzRlODBiNWFlZA==" target="_blank" rel="noopener noreferrer"><img className="shakethis" src={instaImg} alt="Instagram_Profile" /></a></td>
+                        <td><a href="https://www.linkedin.com/company/dsc-iiit-bhopal/" target="_blank" rel="noopener noreferrer"><img className="shakethis" src={linkedInImg} alt="linkedIn_Profile" /></a></td>
+                        <td><a href="https://twitter.com/gdsc_iiitbhopal?t=4NmbYiJ7qfs2eG6RomRGqg&s=09" target="_blank" rel="noopener noreferrer"><img className="shakethis" src={twitterImg} alt="Twitter_profile" /></a></td>
                     </tr>
                 </table>
                 <span>&#169; GDSC IIIT Bhopal</span></center>
--- src/components/HeadingComponents.jsx
+++ src/components/HeadingComponents.jsx
@@ -1,56 +1,44 @@
 import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
-import Component1 from '../images/Component 3.svg'
+import Carousel from 'react-bootstrap/Carousel';
+import Web_dev from '../images/pngs/Web.png'
+import App_Dev from '../images/pngs/app-development.png'
+import Cloud from '../images/pngs/Cloud.png';
+import Data_Sci from '../images/pngs/data.png';
+import AI_ML from '../images/pngs/ml.png';
+import Web3 from '../images/pngs/web-30.png'
+
 
 
 function HeadingComponents() {
     return <div className="headingDiv">
-        <Container>
-            <Row>
-                <Col xs={6} sm={6} md={4} lg={2}><div className="fieldDiv shakethis">
-                    <center>
-                        <img src={Component1} alt="stackIcon" />
-                        <p>Web Dev</p>
-                    </center>
-                </div>
-                </Col>
-                <Col xs={6} sm={6} md={4} lg={2}><div className="fieldDiv shakethis">
-                    <center>
-                        <img src={Component1} alt="stackIcon" />
-                        <p>App Dev</p>
-                    </center>
-                </div>
-                </Col>
-                <Col xs={6} sm={6} md={4} lg={2}><div className="fieldDiv shakethis">
-                    <center>
-                        <img src={Component1} alt="stackIcon" />
-                        <p>Cloud</p>
-                    </center>
-                </div>
-                </Col>
-                <Col xs={6} sm={6} md={4} lg={2}><div className="fieldDiv shakethis">
-                    <center>
-                        <img src={Component1} alt="stackIcon" />
-                        <p>Data Science</p>
-                    </center>
-                </div>
-                </Col>
-                <Col xs={6} sm={6} md={4} lg={2}><div className="fieldDiv shakethis">
-                    <center>
-                        <img src={Component1} alt="stackIcon" />
-                        <p>Ml/AI</p>
-                    </center>
-                </div>
-                </Col>
-                <Col xs={6} sm={6} md={4} lg={2}><div className="fieldDiv shakethis">
-                    <center>
-                        <img src={Component1} alt="stackIcon" />
-                        <p>Web 3.0</p>
-                    </center>
-                </div>
-                </Col>
-            </Row>
-        </Container>
+        <center>
+            <Carousel interval={1000}>
+                <Carousel.Item interval={1000}>
+                    <h2>Web Dev</h2>
+                    <img className='headingCarousel' src={Web_dev} alt='First slide' />
+                </Carousel.Item>
+                <Carousel.Item interval={1000}>
+                    <h2>App Dev</h2>
+                    <img className='headingCarousel' src={App_Dev} alt='Third slide' />
+                </Carousel.Item>
+                <Carousel.Item interval={1000}>
+                    <h2>Cloud Dev</h2>
+                    <img className='headingCarousel' src={Cloud} alt='Third slide' />
+                </Carousel.Item>
+                <Carousel.Item interval={1000}>
+                    <h2>AI/ML</h2>
+                    <img className='headingCarousel' src={AI_ML} alt='Third slide' />
+                </Carousel.Item>
+                <Carousel.Item interval={1000}>
+                    <h2>Data Science</h2>
+                    <img className='headingCarousel' src={Data_Sci} alt='Third slide' />
+                </Carousel.Item>
+                <Carousel.Item interval={1000}>
+                    <h2>Web 3.0</h2>
+                    <img className='headingCarousel' src={Web3} alt='Third slide' />
+                </Carousel.Item>
+            </Carousel>
+        </center>
     </div>
 }
 
